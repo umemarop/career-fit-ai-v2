9e0a5ed7c3895afba744887565f07beb603df6d2
sanghun han <umemarop@gmail.com>
2026-05-29 18:48:02 +1000

    feat(frontend): setup foundation and landing page

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+if (!API_URL) {
+  throw new Error("NEXT_PUBLIC_API_URL is not defined");
+}
+
+export const api = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    return Promise.reject(error);
+  },
+);
